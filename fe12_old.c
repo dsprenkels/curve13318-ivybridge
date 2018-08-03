20b57438d023c6757568802eb3664b064c50a881
Daan Sprenkels <hello@dsprenkels.com>
2018-08-03 16:44:50 +0200

    Optmize the C multiplication code

--- fe12_old.c
+++ fe12_old.c
@@ -125,101 +125,61 @@ void fe12_squeeze(fe12 z)
     z[2] += t1;
 }
 
+static inline double unset_bit59(const double x)
+{
+    union {
+        double f64;
+        uint64_t u64;
+    } tmp = { .f64 = x };
+    tmp.u64 &= 0xF7FFFFFFFFFFFFFF;
+    return tmp.f64;
+}
+
 void fe12_mul(fe12 C, const fe12 A, const fe12 B)
 {
-    double l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10;
-    double h0, h1, h2, h3, h4, h5, h6, h7, h8, h9, h10;
-    double m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10;
+    // Compute L
+    const double l0  = A[0]*B[0];
+    const double l1  = A[0]*B[1] + A[1]*B[0];
+    const double l2  = A[0]*B[2] + A[1]*B[1] + A[2]*B[0];
+    const double l3  = A[0]*B[3] + A[1]*B[2] + A[2]*B[1] + A[3]*B[0];
+    const double l4  = A[0]*B[4] + A[1]*B[3] + A[2]*B[2] + A[3]*B[1] + A[4]*B[0];
+    const double l5  = A[0]*B[5] + A[1]*B[4] + A[2]*B[3] + A[3]*B[2] + A[4]*B[1] + A[5]*B[0];
+    const double l6  =             A[1]*B[5] + A[2]*B[4] + A[3]*B[3] + A[4]*B[2] + A[5]*B[1];
+    const double l7  =                         A[2]*B[5] + A[3]*B[4] + A[4]*B[3] + A[5]*B[2];
+    const double l8  =                                     A[3]*B[5] + A[4]*B[4] + A[5]*B[3];
+    const double l9  =                                                 A[4]*B[5] + A[5]*B[4];
+    const double l10 =                                                             A[5]*B[5];
 
     // Precompute reduced A,B values
+    // For x7-x10, we know for sure that they are:
+    //   - either 0 -> exponent of double is 0b00000000000
+    //   - 0 modulo 2^149 and smaller than 2^237 and as such, the 7'th exponent bit is always set.
+    //     Thus we can use a mask operation for these values to divide them by 2*128.
     const double  A6_shr = 0x1p-128 * A[ 6];
-    const double  A7_shr = 0x1p-128 * A[ 7];
-    const double  A8_shr = 0x1p-128 * A[ 8];
-    const double  A9_shr = 0x1p-128 * A[ 9];
-    const double A10_shr = 0x1p-128 * A[10];
+    const double  A7_shr = unset_bit59(A[7]);
+    const double  A8_shr = unset_bit59(A[8]);
+    const double  A9_shr = unset_bit59(A[9]);
+    const double A10_shr = unset_bit59(A[10]);
     const double A11_shr = 0x1p-128 * A[11];
     const double  B6_shr = 0x1p-128 * B[ 6];
-    const double  B7_shr = 0x1p-128 * B[ 7];
-    const double  B8_shr = 0x1p-128 * B[ 8];
-    const double  B9_shr = 0x1p-128 * B[ 9];
-    const double B10_shr = 0x1p-128 * B[10];
+    const double  B7_shr = unset_bit59(B[7]);
+    const double  B8_shr = unset_bit59(B[8]);
+    const double  B9_shr = unset_bit59(B[9]);
+    const double B10_shr = unset_bit59(B[10]);
     const double B11_shr = 0x1p-128 * B[11];
 
-    // Compute L
-    l0  = A[0] * B[0]; // Round 1/6
-    l1  = A[0] * B[1];
-    l2  = A[0] * B[2];
-    l3  = A[0] * B[3];
-    l4  = A[0] * B[4];
-    l5  = A[0] * B[5];
-    l1 += A[1] * B[0]; // Round 2/6
-    l2 += A[1] * B[1];
-    l3 += A[1] * B[2];
-    l4 += A[1] * B[3];
-    l5 += A[1] * B[4];
-    l6  = A[1] * B[5];
-    l2 += A[2] * B[0]; // Round 3/6
-    l3 += A[2] * B[1];
-    l4 += A[2] * B[2];
-    l5 += A[2] * B[3];
-    l6 += A[2] * B[4];
-    l7  = A[2] * B[5];
-    l3 += A[3] * B[0]; // Round 4/6
-    l4 += A[3] * B[1];
-    l5 += A[3] * B[2];
-    l6 += A[3] * B[3];
-    l7 += A[3] * B[4];
-    l8  = A[3] * B[5];
-    l4 += A[4] * B[0]; // Round 5/6
-    l5 += A[4] * B[1];
-    l6 += A[4] * B[2];
-    l7 += A[4] * B[3];
-    l8 += A[4] * B[4];
-    l9  = A[4] * B[5];
-    l5 += A[5] * B[0]; // Round 6/6
-    l6 += A[5] * B[1];
-    l7 += A[5] * B[2];
-    l8 += A[5] * B[3];
-    l9 += A[5] * B[4];
-    l10 = A[5] * B[5];
-
     // Compute H
-    h0  =  A6_shr *  B6_shr; // Round 1/6
-    h1  =  A6_shr *  B7_shr;
-    h2  =  A6_shr *  B8_shr;
-    h3  =  A6_shr *  B9_shr;
-    h4  =  A6_shr * B10_shr;
-    h5  =  A6_shr * B11_shr;
-    h1 +=  A7_shr *  B6_shr; // Round 2/6
-    h2 +=  A7_shr *  B7_shr;
-    h3 +=  A7_shr *  B8_shr;
-    h4 +=  A7_shr *  B9_shr;
-    h5 +=  A7_shr * B10_shr;
-    h6  =  A7_shr * B11_shr;
-    h2 +=  A8_shr *  B6_shr; // Round 3/6
-    h3 +=  A8_shr *  B7_shr;
-    h4 +=  A8_shr *  B8_shr;
-    h5 +=  A8_shr *  B9_shr;
-    h6 +=  A8_shr * B10_shr;
-    h7  =  A8_shr * B11_shr;
-    h3 +=  A9_shr *  B6_shr; // Round 4/6
-    h4 +=  A9_shr *  B7_shr;
-    h5 +=  A9_shr *  B8_shr;
-    h6 +=  A9_shr *  B9_shr;
-    h7 +=  A9_shr * B10_shr;
-    h8  =  A9_shr * B11_shr;
-    h4 += A10_shr *  B6_shr; // Round 5/6
-    h5 += A10_shr *  B7_shr;
-    h6 += A10_shr *  B8_shr;
-    h7 += A10_shr *  B9_shr;
-    h8 += A10_shr * B10_shr;
-    h9  = A10_shr * B11_shr;
-    h5 += A11_shr *  B6_shr; // Round 6/6
-    h6 += A11_shr *  B7_shr;
-    h7 += A11_shr *  B8_shr;
-    h8 += A11_shr *  B9_shr;
-    h9 += A11_shr * B10_shr;
-    h10 = A11_shr * B11_shr;
+    const double h0  =  A6_shr* B6_shr;
+    const double h1  =  A6_shr* B7_shr + A7_shr* B6_shr;
+    const double h2  =  A6_shr* B8_shr + A7_shr* B7_shr + A8_shr* B6_shr;
+    const double h3  =  A6_shr* B9_shr + A7_shr* B8_shr + A8_shr* B7_shr + A9_shr* B6_shr;
+    const double h4  =  A6_shr*B10_shr + A7_shr* B9_shr + A8_shr* B8_shr + A9_shr* B7_shr + A10_shr* B6_shr;
+    const double h5  =  A6_shr*B11_shr + A7_shr*B10_shr + A8_shr* B9_shr + A9_shr* B8_shr + A10_shr* B7_shr + A11_shr* B6_shr;
+    const double h6  =                   A7_shr*B11_shr + A8_shr*B10_shr + A9_shr* B9_shr + A10_shr* B8_shr + A11_shr* B7_shr;
+    const double h7  =                                    A8_shr*B11_shr + A9_shr*B10_shr + A10_shr* B9_shr + A11_shr* B8_shr;
+    const double h8  =                                                     A9_shr*B11_shr + A10_shr*B10_shr + A11_shr* B9_shr;
+    const double h9  =                                                                      A10_shr*B11_shr + A11_shr*B10_shr;
+    const double h10 =                                                                                        A11_shr*B11_shr;
 
     // Compute M_hat
     const double mA0 = (A[0] -  A6_shr);
@@ -228,62 +188,37 @@ void fe12_mul(fe12 C, const fe12 A, const fe12 B)
     const double mA3 = (A[3] -  A9_shr);
     const double mA4 = (A[4] - A10_shr);
     const double mA5 = (A[5] - A11_shr);
-    const double mB0 = (B[0] -  B6_shr);
-    const double mB1 = (B[1] -  B7_shr);
-    const double mB2 = (B[2] -  B8_shr);
-    const double mB3 = (B[3] -  B9_shr);
-    const double mB4 = (B[4] - B10_shr);
-    const double mB5 = (B[5] - B11_shr);
-    m0  = mA0 * mB0; // Round 1/6
-    m1  = mA0 * mB1;
-    m2  = mA0 * mB2;
-    m3  = mA0 * mB3;
-    m4  = mA0 * mB4;
-    m5  = mA0 * mB5;
-    m1 += mA1 * mB0; // Round 2/6
-    m2 += mA1 * mB1;
-    m3 += mA1 * mB2;
-    m4 += mA1 * mB3;
-    m5 += mA1 * mB4;
-    m6  = mA1 * mB5;
-    m2 += mA2 * mB0; // Round 3/6
-    m3 += mA2 * mB1;
-    m4 += mA2 * mB2;
-    m5 += mA2 * mB3;
-    m6 += mA2 * mB4;
-    m7  = mA2 * mB5;
-    m3 += mA3 * mB0; // Round 4/6
-    m4 += mA3 * mB1;
-    m5 += mA3 * mB2;
-    m6 += mA3 * mB3;
-    m7 += mA3 * mB4;
-    m8  = mA3 * mB5;
-    m4 += mA4 * mB0; // Round 5/6
-    m5 += mA4 * mB1;
-    m6 += mA4 * mB2;
-    m7 += mA4 * mB3;
-    m8 += mA4 * mB4;
-    m9  = mA4 * mB5;
-    m5 += mA5 * mB0; // Round 6/6
-    m6 += mA5 * mB1;
-    m7 += mA5 * mB2;
-    m8 += mA5 * mB3;
-    m9 += mA5 * mB4;
-    m10 = mA5 * mB5;
+    const double mB0 = ( B6_shr - B[0]);
+    const double mB1 = ( B7_shr - B[1]);
+    const double mB2 = ( B8_shr - B[2]);
+    const double mB3 = ( B9_shr - B[3]);
+    const double mB4 = (B10_shr - B[4]);
+    const double mB5 = (B11_shr - B[5]);
+    const double m0  = mA0 * mB0;
+    const double m1  = mA0 * mB1 + mA1 * mB0;
+    const double m2  = mA0 * mB2 + mA1 * mB1 + mA2 * mB0;
+    const double m3  = mA0 * mB3 + mA1 * mB2 + mA2 * mB1 + mA3 * mB0;
+    const double m4  = mA0 * mB4 + mA1 * mB3 + mA2 * mB2 + mA3 * mB1 + mA4 * mB0;
+    const double m5  = mA0 * mB5 + mA1 * mB4 + mA2 * mB3 + mA3 * mB2 + mA4 * mB1 + mA5 * mB0;
+    const double m6  =             mA1 * mB5 + mA2 * mB4 + mA3 * mB3 + mA4 * mB2 + mA5 * mB1;
+    const double m7  =                         mA2 * mB5 + mA3 * mB4 + mA4 * mB3 + mA5 * mB2;
+    const double m8  =                                     mA3 * mB5 + mA4 * mB4 + mA5 * mB3;
+    const double m9  =                                                 mA4 * mB5 + mA5 * mB4;
+    const double m10 =                                                             mA5 * mB5;
 
     // Sum up the accs into C
-    C[ 0] =  l0 + 0x26p-128 * ( -m6 +  l6 +  h6) + 0x26*h0;
-    C[ 1] =  l1 + 0x26p-128 * ( -m7 +  l7 +  h7) + 0x26*h1;
-    C[ 2] =  l2 + 0x26p-128 * ( -m8 +  l8 +  h8) + 0x26*h2;
-    C[ 3] =  l3 + 0x26p-128 * ( -m9 +  l9 +  h9) + 0x26*h3;
-    C[ 4] =  l4 + 0x26p-128 * (-m10 + l10 + h10) + 0x26*h4;
-    C[ 5] =  l5                                  + 0x26*h5;
-    C[ 6] =  l6 +  0x1p+128 * ( -m0 +  l0 +  h0) + 0x26*h6;
-    C[ 7] =  l7 +  0x1p+128 * ( -m1 +  l1 +  h1) + 0x26*h7;
-    C[ 8] =  l8 +  0x1p+128 * ( -m2 +  l2 +  h2) + 0x26*h8;
-    C[ 9] =  l9 +  0x1p+128 * ( -m3 +  l3 +  h3) + 0x26*h9;
-    C[10] = l10 +  0x1p+128 * ( -m4 +  l4 +  h4) + 0x26*h10;
-    C[11] =        0x1p+128 * ( -m5 +  l5 +  h5);
+    C[ 0] =  l0 + 0x26 * (0x1p-128 * ( m6 +  l6 +  h6) + h0);
+    C[ 1] =  l1 + 0x26 * (0x1p-128 * ( m7 +  l7 +  h7) + h1);
+    C[ 2] =  l2 + 0x26 * (0x1p-128 * ( m8 +  l8 +  h8) + h2);
+    C[ 3] =  l3 + 0x26 * (0x1p-128 * ( m9 +  l9 +  h9) + h3);
+    C[ 4] =  l4 + 0x26 * (0x1p-128 * (m10 + l10 + h10) + h4);
+    C[ 5] =  l5 + 0x26 *                                 h5 ;
+    C[ 6] =  l6 + 0x1p+128 * ( m0 + l0 + h0) + 0x26*h6;
+    C[ 7] =  l7 + 0x1p+128 * ( m1 + l1 + h1) + 0x26*h7;
+    C[ 8] =  l8 + 0x1p+128 * ( m2 + l2 + h2) + 0x26*h8;
+    C[ 9] =  l9 + 0x1p+128 * ( m3 + l3 + h3) + 0x26*h9;
+    C[10] = l10 + 0x1p+128 * ( m4 + l4 + h4) + 0x26*h10;
+    C[11] =       0x1p+128 * ( m5 + l5 + h5);
     fe12_squeeze(C);
 }
 
